892630bf20a4b5450bd8b88cb97784e43ee85279
Hailee <hjwheat@cpp.edu>
2023-11-26 01:00:16 -0800

    Everything is Good!! Just needs to be pretty and give correct results. All layout is done, a little more logic to be MVP.

---
+++ app/src/main/java/com/example/loginfrd/QuizActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.loginfrd
+
+data class QuizActivityViewModel(val question:String,
+                                 val option1:String,
+                                 val option2:String,
+                                 val option3:String,
+                                 val option4:String)
